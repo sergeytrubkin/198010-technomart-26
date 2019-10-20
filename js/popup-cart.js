f9a855606ef3c94a866c0d58755b2a8b94af4249
Sergei <trubkinsa@gmail.com>
2019-10-20 18:49:25 +0300

    минификация css и js файлов

--- js/popup-cart.js
+++ js/popup-cart.js
@@ -1,9 +1,7 @@
 // popup-cart
 var cartLink = document.querySelectorAll(".catalog-items__buy");
 var cartPopup = document.querySelector(".popup-cart");
-var cartClose = document.querySelector(".modal__close");
-
-console.log(cartLink);
+var cartClose = document.querySelectorAll(".modal__close");
 
 for (var i = 0; i < cartLink.length; i++) {
   cartLink[i].addEventListener("click", function(evt) {
@@ -12,10 +10,12 @@ for (var i = 0; i < cartLink.length; i++) {
   });
 }
 
-cartClose.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  cartPopup.classList.remove("modal-show");
-});
+for (var i = 0; i < cartClose.length; i++) {
+  cartClose[i].addEventListener("click", function(evt) {
+    evt.preventDefault();
+    cartPopup.classList.remove("modal-show");
+  });
+}
 
 // закрывается при нажатии на форму!!!
 // cartPopup.addEventListener("click", function(evt) {
