67c460a572fd6bae8f5324e34982b07a52c30b3f
Sergei <trubkinsa@gmail.com>
2019-10-20 19:34:53 +0300

    доработка js
    Добавил активацию вкладок "корзина" и "вкладки".

--- js/popup-cart.js
+++ js/popup-cart.js
@@ -1,19 +1,35 @@
 // popup-cart
 var cartLink = document.querySelectorAll(".catalog-items__buy");
+var cartBookmark = document.querySelectorAll(".catalog-items__bookmarks");
+
 var cartPopup = document.querySelector(".popup-cart");
 var cartClose = document.querySelectorAll(".modal__close");
 
+var bookmarkTab = document.querySelector(".header-top__item--bookmark");
+var cartTab = document.querySelector(".header-top__item--cart");
+
+// открытие попапа и активация вкладки "корзина"
 for (var i = 0; i < cartLink.length; i++) {
   cartLink[i].addEventListener("click", function(evt) {
     evt.preventDefault();
     cartPopup.classList.add("modal-show");
+    cartTab.classList.add("header-top__item--active");
   });
 }
 
+// закрытие попапа
 for (var i = 0; i < cartClose.length; i++) {
   cartClose[i].addEventListener("click", function(evt) {
     evt.preventDefault();
     cartPopup.classList.remove("modal-show");
+  });
+}
+
+// активация вкладки "закладки"
+for (var i = 0; i < cartBookmark.length; i++) {
+  cartBookmark[i].addEventListener("click", function(evt) {
+    evt.preventDefault();
+    bookmarkTab.classList.add("header-top__item--active");
   });
 }
 
